da2af39a302cea7dbc564150276555a5b91ed06c
Vivian Nguyen <vivian.nguyen1127@gmail.com>
2021-09-30 14:50:43 -0700

    Import client, choose random member from a channel.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 import logging
+import random 
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -9,6 +10,8 @@ from slack_bolt.adapter.socket_mode import SocketModeHandler
 
 from connectinator.connect_command import ConnectCommand
 
+from slackclient import SlackClient
+
 load_dotenv()
 
 SLACK_BOT_TOKEN = os.getenv("SLACK_BOT_TOKEN")
@@ -16,6 +19,18 @@ SLACK_APP_TOKEN = os.getenv("SLACK_APP_TOKEN")
 
 app = App(token=SLACK_BOT_TOKEN)
 
+client = SlackClient(SLACK_BOT_TOKEN)
+memberRequest = client.api_call("users.list")
+if memberRequest['ok']:
+    randomMember = random.choice(list(memberRequest['members']))
+    while randomMember['is_bot'] == True:
+        randomMember = random.choice(list(memberRequest['members']))
+    print(randomMember['name'])
+    # for member in memberRequest['members']:
+    #     print(member)
+        
+ 
+        
 
 @app.message("hello")
 def message_hello(message, say):
