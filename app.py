b49251558029280b5372a9db45ef210112986232
Vivian Nguyen <vivian.nguyen1127@gmail.com>
2021-10-01 13:18:53 -0700

    Converted csv to json, add random question chooser to reaction listener

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 import os
 import logging
 import random 
+import json
 
 logging.basicConfig(level=logging.DEBUG)
 
 from dotenv import load_dotenv
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
+from collections import defaultdict
 
 from connectinator.connect_command import ConnectCommand
 
@@ -25,19 +27,41 @@ if memberRequest['ok']:
     randomMember = random.choice(list(memberRequest['members']))
     while randomMember['is_bot'] == True:
         randomMember = random.choice(list(memberRequest['members']))
-    logging.debug(f"random member = {randomMember['name']}")
+    # logging.debug(f"random member = {randomMember['name']}")
+
+
+
 
 
 @app.event("reaction_added")
 def track_question_level(event, say):
-    print(event)
-    print("Reaction detected")
+    questionBank = open('QUESTION_BANK.json', mode='r', encoding='utf-8-sig')
+    questionBankDict = json.load(questionBank)
+    questions_by_type_dict = defaultdict(list)
+    for quesiton in questionBankDict:
+        questions_by_type_dict[questionBankDict[0]].append(quesiton)
+
+    print(questions_by_type_dict)
+    print("Reaction detected -----------------")
+    # question = random.choice(list(questionBankDict))
+    # if event['reaction'] == 'relaxed':
+    #     # Choose question with type 0, level 0
+    #     while question['QUESTION_TYPE'] != 0 and question['LEVEL'] != 0:
+    #         question = random.choice(list(questionBankDict))
+    #     # Send question body
+    # elif event['reaction'] == 'hugging_face':
+    #     # Choose question with type 0, level 1
+    #     while question['QUESTION_TYPE'] != 0 and question['LEVEL'] != 1:
+    #         question = random.choice(list(questionBankDict))
+        # Send question body
+    # print(question)
+    questionBank.close()
     say(event)
 
 
 @app.message("hello")
 def message_hello(message, say):
-    logging.debug("someone said hello")
+    # logging.debug("someone said hello")
     say(f"Hey there <@{message['user']}>!")
 
 
@@ -50,6 +74,9 @@ def connect(ack, client, say, command):
 @app.event("message")
 def handle_message_events(body, logger):
     logger.info(body)
+
+
+
 
 if __name__ == "__main__":
     SocketModeHandler(app, SLACK_APP_TOKEN).start()
